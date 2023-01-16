1a8a3034909093d7b65db379e63fe54b08c0f2c2
Max Taylor <maxtaylor0804@gmail.com>
2023-01-16 11:47:48 +0100

    la till objekt

--- app.ts
+++ app.ts
@@ -1,2 +1,10 @@
 let arr: string[] = ["apa", "orangutang", "gorilla", "get", "giraff"];
-console.log(arr)
+console.log(arr);
+
+const person: {name: string, age: number, crazy: boolean } = {
+    name: "Max",
+    age: 23,
+    crazy: true
+};
+console.log(person);
+
